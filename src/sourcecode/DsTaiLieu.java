fd7a0e2b6dadba77e27bcfc18b5c687944678caf
thaola131 <1tran23a@gmail.com>
2024-11-20 15:19:56 +0700

    Update DsTaiLieu.java
    abc

--- src/sourcecode/DsTaiLieu.java
+++ src/sourcecode/DsTaiLieu.java
@@ -88,11 +88,72 @@ public class DsTaiLieu{
                 found = true;
             }
         }
-        if (!found) {
-            System.out.println("Khong co tai lieu nao cua tac gia: " + tenTG);
+import java.util.Scanner;
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DsTaiLieu{
+    private TaiLieu[] dsTL;
+    private int soluongTL;
+    
+    public DsTaiLieu(int kichThuoc){
+        dsTL = new TaiLieu[kichThuoc];
+        soluongTL = 0;
+    }
+    
+    public void themTL(TaiLieu tl){
+        if (soluongTL < dsTL.length){
+        	tl.nhap();
+            dsTL[soluongTL] = tl;
+            soluongTL++;
+            ghiDuLieuRaFile("tailieu.txt");
+        } else {
+            System.out.println("Danh sach day");
         }
     }
 
+    public void suaTL(String maTL){
+        for (int i=0; i<soluongTL; i++){
+            if(dsTL[i].getMaTL().equals(maTL)){
+                System.out.println("Nhap lai thong tin tai lieu: ");
+                dsTL[i].nhap();
+                ghiDuLieuRaFile("tailieu.txt");
+                break;
+            }
+        }
+    }
+
+    public void xoaTL(String maTL){
+        for(int i= 0; i< soluongTL;i++){
+            if(dsTL[i].getMaTL().equals(maTL)){
+                for(int j=i; j< soluongTL-1; j++){
+                    dsTL[j]= dsTL[j+1];
+                }
+                dsTL[soluongTL- 1]= null;
+                soluongTL--;
+                ghiDuLieuRaFile("tailieu.txt");
+                break;
+            }
+        }
+    }
+    public void timKiemtheoMa(String maTL) {
+        boolean found = false;
+        for (int i = 0; i < soluongTL; i++) {
+            if (dsTL[i].getMaTL().equals(maTL)) {
+                dsTL[i].xuat();
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            System.out.println("Tai lieu khong tim thay.");
+        }
+    }
+    
     public void timKiemTheoTen(String tenTL) {
         boolean found = false;
         for (int i = 0; i < soluongTL; i++) {
@@ -101,14 +162,64 @@ public class DsTaiLieu{
                 found = true;
             }
         }
-        if (!found) {
+        if (!found){
             System.out.println("Khong tim thay tai lieu voi ten: " + tenTL);
         }
     }
     public void xuat_ds(){
+    	if(soluongTL ==0) {
+    		System.out.println("Danh sach rong");
+    		return;
+    	}
+    	System.out.println("Danh sach tai lieu: ");
         for(int i = 0; i < soluongTL; i++){
             dsTL[i].xuat();
-            System.out.println();
+        }
+    }
+    
+    public TaiLieu[] getDanhSachTaiLieu() {
+        return dsTL;
+    }
+
+    public int getSoLuongTaiLieu() {
+        return soluongTL;
+    }
+    
+    private void ghiDuLieuRaFile(String tenFile){
+    	String duongDan = "C:\\Users\\Admin\\Documents\\NetBeansProjects\\DOAN\\src\\DoAn_QLTV_main\\src\\sourcefile\\" + tenFile;
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(duongDan))) {
+            for (int i = 0; i < soluongTL; i++) {
+                writer.write(dsTL[i].toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Lỗi khi ghi vào file: " + e.getMessage());
+        }
+    }
+
+    private void docDuLieuTuFile(String tenFile){
+    	String duongDan = "C:\\Users\\Admin\\Documents\\NetBeansProjects\\DOAN\\src\\DoAn_QLTV_main\\src\\sourcefile\\" + tenFile;
+        try (BufferedReader reader = new BufferedReader(new FileReader(duongDan))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(" ");
+                if (data.length == 5) {
+                    TaiLieu tl = new TaiLieu();
+                    TacGia tg = new TacGia();
+                    TheLoai tloai = new TheLoai();
+                    tl.setMaTL(data[0]);
+                    tl.setTenTL(data[1]);
+                    tloai.setTenTLoai(data[2]);
+                    tl.setTheLoai(tloai);
+                    tg.setTenTG(data[3]);
+                    tl.setTacGia(tg);
+                    tl.setTenNXB(data[4]);
+                    
+                    themTL(tl);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Lỗi khi đọc từ file: " + e.getMessage());
         }
     }
 }
