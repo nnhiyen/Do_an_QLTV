5d14110c351b9e1bd8f4495384be32f8a65552d4
thaola131 <1tran23a@gmail.com>
2024-12-01 18:20:34 +0700

    Update TacGia.java

--- src/sourcecode/TacGia.java
+++ src/sourcecode/TacGia.java
@@ -3,7 +3,6 @@ package DoAn_QLTV_main.src.sourcecode;
 import java.util.Scanner;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class TacGia{
 	private String tenTG;
